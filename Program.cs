9bf293a5638b008d17f56c0552b31a85dd5d7297
Rodrigo91218 <pereira97rodrigo@gmail.com>
2026-04-10 13:40:57 -0300

    creo que ya es a prueba de errores y con funciones

--- Program.cs
+++ Program.cs
@@ -17,25 +17,14 @@ namespace Actividad_1
             double mayor = 0; // Variable double para que el usuario puedo colocar cualquier tipo de dato númerico (int, float, decimal, double, etc.)
             int cantMayor = 0; // Variable que sirva como contador para saber la cantidad de veces que se repitio el mayor  valor.
             Console.WriteLine("Ingrese 10 números");
-            Console.Write($"Número {varN}: ");
-            while (!double.TryParse(Console.ReadLine(), out vec[i])) // Validador por si el usuario ingresa enter o string.
-            {
-                Console.WriteLine("Error!. Ingrese solo valores numéricos.");
-                Console.Write($"Número {varN}: ");
-
-            }
+            vec[i] = LeerNumero($"Número {varN}: ");
             mayor = vec[i];// El primer valor que se ingresa es el mayor. Esto para evitar errores más adelante.
             cantMayor++;// Se eleva en 1 la cantidad de veces que se repite el mayor valor
             varN++;
             i++; // Se desplaza el casillero del vector al siguiente del 0 al 1.
             for (i = 1; i < vec.Length; i++) //Bucle for para repetir la misma secuencia de ingreso de valores y que coincida con el tamaño del vector.
             {
-                Console.Write($"Número {varN}: ");
-                while (!double.TryParse(Console.ReadLine(), out vec[i])) 
-                {
-                    Console.WriteLine("Error!. Ingrese solo valores numéricos: ");
-                    Console.Write($"Número {varN}: ");
-                }
+                vec[i] = LeerNumero($"Número {varN}: ");
                 if (vec[i] == mayor)// Si el valor del vector en la posición 'i' es igual al valor en la variable 'mayor' se sumara el valor de 1 en la variable 'cantMayor'.
                 {
                     cantMayor++;
@@ -61,9 +50,24 @@ namespace Actividad_1
                 Console.WriteLine($"\nEl número mayor que se ingreso fue: {mayor} y se repitió {cantMayor} veces.");// Se muestran los resultados finales. Interpolación para colocar variables más facil.
                 Console.ReadKey();//Pausa la consola para que no se cierre al terminar el programa.
             }
-            
-           
-            
+
+
+
+            double LeerNumero(string mensaje)
+            {
+                double valor;
+
+                Console.Write(mensaje);
+
+                while (!double.TryParse(Console.ReadLine(), out valor))
+                {
+                    Console.WriteLine("Error!. Ingrese solo valores numéricos.");
+                    Console.Write(mensaje);
+                }
+
+                return valor;
+            }
+
         }
     }
 }
